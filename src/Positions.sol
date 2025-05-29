77f944caf645f20d6c507b61081bf0a03ebbfba6
Jeff Huang <jeffishjeff@gmail.com>
2025-05-29 09:44:03 +0800

    IPositions

--- src/Positions.sol
+++ src/Positions.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.30;
+pragma solidity =0.8.30;
 
 import {IPositions} from "./interfaces/IPositions.sol";
 import {Fungible} from "./types/Fungible.sol";
