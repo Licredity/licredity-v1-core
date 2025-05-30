7f57be5856d7f9af6e993d3b698410f3b64f3b17
Jeff Huang <jeffishjeff@gmail.com>
2025-05-30 17:52:39 +0800

    deposit fungible

--- src/types/Fungible.sol
+++ src/types/Fungible.sol
@@ -1,4 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+import {IERC20} from "@forge-std/interfaces/IERC20.sol";
+
 type Fungible is address;
+
+using {equals as ==} for Fungible global;
+using FungibleLibrary for Fungible global;
+
+function equals(Fungible x, Fungible y) pure returns (bool) {
+    return Fungible.unwrap(x) == Fungible.unwrap(y);
+}
+
+/// @title FungibleLibrary
+/// @notice Library for managing fungibles
+library FungibleLibrary {
+    /// @notice A constant representing the native fungible
+    Fungible public constant NATIVE = Fungible.wrap(address(0));
+
+    /// @notice Get the balance of a fungible for an address
+    /// @param self The fungible to check the balance of
+    /// @param owner The address to check the balance for
+    /// @return uint256 The balance of the fungible for the address
+    function balanceOf(Fungible self, address owner) internal view returns (uint256) {
+        if (self.isNative()) {
+            return owner.balance;
+        } else {
+            return IERC20(Fungible.unwrap(self)).balanceOf(owner);
+        }
+    }
+
+    /// @notice Checks if the fungible is the native fungible
+    /// @param self The fungible to check
+    /// @return bool True if the fungible is the native fungible, false otherwise
+    function isNative(Fungible self) internal pure returns (bool) {
+        return self == NATIVE;
+    }
+}
--- src/types/FungibleState.sol
+++ src/types/FungibleState.sol
@@ -2,3 +2,48 @@
 pragma solidity >=0.8.0;
 
 type FungibleState is bytes32;
+
+using FungibleStateLibrary for FungibleState global;
+
+function toFungibleState(uint64 index, uint192 _balance) pure returns (FungibleState state) {
+    assembly ("memory-safe") {
+        state := or(shl(192, index), and(_balance, sub(shl(192, 1), 1)))
+    }
+}
+
+/// @title FungibleStateLibrary
+/// @notice Library for managing fungible states
+library FungibleStateLibrary {
+    /// @notice Get the index part of the fungible state
+    /// @param self The fungible state to get the index part of
+    /// @return _index The index part of the fungible state
+    function index(FungibleState self) internal pure returns (uint64 _index) {
+        assembly ("memory-safe") {
+            _index := shr(192, self)
+        }
+    }
+
+    /// @notice Get the balance part of the fungible state
+    /// @param self The fungible state to get the balance part of
+    /// @return _balance The balance part of the fungible state
+    function balance(FungibleState self) internal pure returns (uint192 _balance) {
+        assembly ("memory-safe") {
+            _balance := and(self, sub(shl(192, 1), 1))
+        }
+    }
+
+    /// @notice Check if the fungible state is empty
+    /// @param self The fungible state to check
+    /// @return bool True if the fungible state is empty, false otherwise
+    function isEmpty(FungibleState self) internal pure returns (bool) {
+        return FungibleState.unwrap(self) == 0;
+    }
+
+    /// @notice Add an amount to the balance of the fungible state
+    /// @param self The fungible state to add the amount to
+    /// @param amount The amount to add
+    /// @return FungibleState The updated fungible state
+    function add(FungibleState self, uint192 amount) internal pure returns (FungibleState) {
+        return toFungibleState(self.index(), self.balance() + amount);
+    }
+}
