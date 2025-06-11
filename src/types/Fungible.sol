516751bc776c674be5b0ad4094229e1adb055938
Jeff Huang <jeffishjeff@gmail.com>
2025-06-11 20:57:21 +0800

    fungible transfer

--- src/types/Fungible.sol
+++ src/types/Fungible.sol
@@ -15,6 +15,12 @@ function equals(Fungible x, Fungible y) pure returns (bool) {
 /// @title FungibleLibrary
 /// @notice Library for managing fungibles
 library FungibleLibrary {
+    /// @notice Thrown when a native transfer fails
+    error NativeTransferFailed();
+
+    /// @notice Thrown when an ERC20 transfer fails
+    error ERC20TransferFailed();
+
     /// @notice A constant representing the native fungible
     Fungible public constant NATIVE = Fungible.wrap(address(0));
 
@@ -23,7 +29,38 @@ library FungibleLibrary {
     /// @param amount The amount of fungible to transfer
     /// @param recipient The address to transfer the fungible to
     function transfer(Fungible self, uint256 amount, address recipient) internal {
-        // TODO: implement
+        if (self.isNative()) {
+            assembly ("memory-safe") {
+                let success := call(gas(), recipient, amount, 0, 0, 0, 0)
+
+                if iszero(success) {
+                    mstore(0x00, 0xf4b3b1bc) // 'NativeTransferFailed()'
+                    revert(0x1c, 0x04)
+                }
+            }
+        } else {
+            assembly ("memory-safe") {
+                let fmp := mload(0x40)
+                mstore(fmp, 0xa9059cbb00000000000000000000000000000000000000000000000000000000) // "transfer(address,uint256)"
+                mstore(add(fmp, 0x04), and(recipient, 0xffffffffffffffffffffffffffffffffffffffff))
+                mstore(add(fmp, 0x24), amount)
+
+                let success :=
+                    and(
+                        or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                        call(gas(), self, 0, fmp, 68, 0, 32)
+                    )
+
+                mstore(fmp, 0)
+                mstore(add(fmp, 0x20), 0)
+                mstore(add(fmp, 0x40), 0)
+
+                if iszero(success) {
+                    mstore(0x00, 0xf27f64e4) // 'ERC20TransferFailed()'
+                    revert(0x1c, 0x04)
+                }
+            }
+        }
     }
 
     /// @notice Get the balance of a fungible for an address
