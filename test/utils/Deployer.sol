2a3047305f9e8ef7d68650da41ca68802d027a18
Wong Shouhao <wongshouhao@outlook.com>
2025-06-14 07:42:43 +0800

    Add debt test (#22)
    * Fix state library
    
    * Add router and router helper
    
    * Add addDebt test
    
    * Finish add debt

--- test/utils/Deployer.sol
+++ test/utils/Deployer.sol
@@ -5,17 +5,42 @@ import {Test} from "@forge-std/Test.sol";
 import {Licredity} from "src/Licredity.sol";
 import {NonFungible} from "src/types/NonFungible.sol";
 import {NonFungibleMock} from "test/mocks/NonFungibleMock.sol";
+import {OracleMock} from "test/mocks/OracleMock.sol";
+import {LicredityRouter} from "./LicredityRouter.sol";
+import {LicredityRouterHelper} from "./LicredityRouterHelper.sol";
+import {IPoolManager} from "@uniswap-v4-core/interfaces/IPoolManager.sol";
 import {TestERC20} from "@uniswap-v4-core/test/TestERC20.sol";
 
 contract Deployers is Test {
+    IPoolManager public poolManager;
     address constant UNISWAP_V4 = address(0x000000000004444c5dc75cB358380D2e3dE08A90);
+
     Licredity public licredity;
     NonFungibleMock public nonFungibleMock;
+
     TestERC20 public fungibleMock;
     TestERC20 public otherFungibleMock;
 
-    function deployFreshETHLicredity() public {
-        licredity = new Licredity(address(0), UNISWAP_V4, "Debt ETH", "DETH", address(this));
+    LicredityRouter public licredityRouter;
+    LicredityRouterHelper public licredityRouterHelper;
+
+    function deployETHLicredityWithUniswapV4() public {
+        vm.createSelectFork("ETH", 22638094);
+        poolManager = IPoolManager(address(0x000000000004444c5dc75cB358380D2e3dE08A90));
+
+        address mockLicredity = address(0xFb46d30c9B3ACc61d714D167179748FD01E09aC0);
+
+        vm.label(mockLicredity, "Licredity");
+        deployCodeTo(
+            "Licredity.sol", abi.encode(address(0), UNISWAP_V4, "Debt ETH", "DETH", address(this)), mockLicredity
+        );
+
+        licredity = Licredity(mockLicredity);
+    }
+
+    function deployLicredityRouter() public {
+        licredityRouter = new LicredityRouter(licredity);
+        licredityRouterHelper = new LicredityRouterHelper(licredityRouter);
     }
 
     function deployNonFungibleMock() public {
@@ -25,6 +50,13 @@ contract Deployers is Test {
     function deployFungibleMock() public {
         fungibleMock = new TestERC20(0);
         otherFungibleMock = new TestERC20(0);
+    }
+
+    function deployAndSetOracleMock() public {
+        OracleMock oracleMock = new OracleMock();
+        oracleMock.setFungibleConfig(address(0), 1 ether, 10); // 10 / 10000 = 0.1%
+        oracleMock.setFungibleConfig(address(licredity), 1 ether, 0); // 10 / 10000 = 0.1%
+        licredity.setOracle(address(oracleMock));
     }
 
     function getMockFungible(uint256 tokenId) public view returns (NonFungible nft) {
---
+++ test/utils/LicredityRouterHelper.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {LicredityRouter, Actions} from "./LicredityRouter.sol";
+
+contract LicredityRouterHelper {
+    LicredityRouter internal router;
+
+    constructor(LicredityRouter router_) {
+        router = router_;
+    }
+
+    function addDebt(uint256 positionId, uint256 share, address recipient) external {
+        Actions[] memory actions = new Actions[](1);
+        bytes[] memory params = new bytes[](1);
+
+        actions[0] = Actions.ADD_DEBT;
+        params[0] = abi.encode(positionId, share, recipient);
+
+        router.executeActions(actions, params);
+    }
+}
