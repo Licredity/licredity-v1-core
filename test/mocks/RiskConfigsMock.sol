bdc6937ef337056c0b8bc273a4a81a349372af0e
Wong Shouhao <wongshouhao@outlook.com>
2025-06-16 00:42:53 +0800

    Add risk config test (#27)

--- test/mocks/RiskConfigsMock.sol
+++ test/mocks/RiskConfigsMock.sol
@@ -1,24 +1,32 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity =0.8.30;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.30;
 
-// import {RiskConfigs} from "src/RiskConfigs.sol";
+import {RiskConfigs} from "src/RiskConfigs.sol";
 
-// contract RiskConfigsMock is RiskConfigs {
-//     constructor(address _governor) RiskConfigs(_governor) {}
+contract RiskConfigsMock is RiskConfigs {
+    constructor(address _governor) RiskConfigs(_governor) {}
 
-//     function loadGovernor() external view returns (address) {
-//         return governor;
-//     }
+    function loadGovernor() external view returns (address) {
+        return governor;
+    }
 
-//     function loadPendingGovernor() external view returns (address) {
-//         return pendingGovernor;
-//     }
+    function loadNextGovernor() external view returns (address) {
+        return nextGovernor;
+    }
 
-//     function loadOracle() external view returns (address) {
-//         return address(oracle);
-//     }
+    function loadOracle() external view returns (address) {
+        return address(oracle);
+    }
 
-//     function loadPositionMrrBps() external view returns (uint16) {
-//         return positionMrrBps;
-//     }
-// }
+    function loadProtocolFeeRecipient() external view returns (address) {
+        return protocolFeeRecipient;
+    }
+
+    function loadProtocolFeePips() external view returns (uint24) {
+        return protocolFeePips;
+    }
+
+    function loadPositionMrrPips() external view returns (uint24) {
+        return positionMrrPips;
+    }
+}
