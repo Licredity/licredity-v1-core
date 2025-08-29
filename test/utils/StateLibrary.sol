ff11714cc9b4e70e1021308acd97c2c7da01a110
wangshouh <wongshouhao@outlook.com>
2025-08-29 20:15:06 +0700

    Add state read test

--- test/utils/StateLibrary.sol
+++ test/utils/StateLibrary.sol
@@ -5,10 +5,30 @@ import {ILicredity} from "src/interfaces/ILicredity.sol";
 import {FungibleState} from "src/types/FungibleState.sol";
 
 library StateLibrary {
+    uint256 public constant POOL_MANAGER_OFFSET = 5;
+    uint256 public constant CURRENCY0_OFFSET = 13;
     uint256 public constant TOTAL_DEBT_SHARE_OFFSET = 16;
     uint256 public constant TOTAL_DEBT_BALANCE_OFFSET = 17;
     uint256 public constant POSITIONS_OFFSET = 24;
     uint256 public constant FUNGIBLES_STATE_OFFSET = 4;
+
+    uint24 private constant FEE = 100;
+    int24 private constant TICK_SPACING = 1;
+
+    function getPoolId(ILicredity manager) internal view returns (bytes32 poolId) {
+        bytes32 currency0 = manager.extsload(bytes32(CURRENCY0_OFFSET));
+        assembly ("memory-safe") {
+            let memptr := mload(0x40)
+            mstore(memptr, currency0) // currency0
+            mstore(add(memptr, 0x20), manager) // currency1
+            mstore(add(memptr, 0x40), FEE) // fee
+            mstore(add(memptr, 0x60), TICK_SPACING) // tickSpacing
+            mstore(add(memptr, 0x80), manager) // hooks
+            poolId := keccak256(memptr, 0xa0)
+
+            mstore(0x40, add(memptr, 0xa0)) // update free memory pointer
+        }
+    }
 
     function getTotalDebt(ILicredity manager) internal view returns (uint256 totalShares, uint256 totalAssets) {
         totalShares = uint256(manager.extsload(bytes32(TOTAL_DEBT_SHARE_OFFSET)));
