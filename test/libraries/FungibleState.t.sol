8def30a5fe77ab0c6e9cdc910b0deb54fd795f2f
Jeff Huang <jeffishjeff@gmail.com>
2025-09-14 11:13:11 +0800

    FungibleState.from test

--- test/libraries/FungibleState.t.sol
+++ test/libraries/FungibleState.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {Test} from "@forge-std/Test.sol";
+import {ILicredity} from "src/interfaces/ILicredity.sol";
 import {FungibleState, FungibleStateLibrary} from "src/types/FungibleState.sol";
 
 contract FungibleStateTest is Test {
@@ -9,5 +10,25 @@ contract FungibleStateTest is Test {
         FungibleState state = FungibleStateLibrary.from(index, balance);
         assertEq(state.index(), index);
         assertEq(state.balance(), balance);
+    }
+
+    function test_fuzz_from(uint256 index, uint256 balance) public {
+        if (index > type(uint64).max) {
+            vm.expectRevert(ILicredity.MaxFungibleIndexExceeded.selector);
+        } else if (balance > type(uint128).max) {
+            vm.expectRevert(ILicredity.MaxFungibleBalanceExceeded.selector);
+        }
+
+        (, bytes memory data) = address(this).call{value: 0}(abi.encodeCall(this.from, (index, balance)));
+        FungibleState state = abi.decode(data, (FungibleState));
+
+        if (index <= type(uint64).max && balance <= type(uint128).max) {
+            assertEq(state.index(), index);
+            assertEq(state.balance(), balance);
+        }
+    }
+
+    function from(uint256 index, uint256 balance) public returns (FungibleState) {
+        return FungibleStateLibrary.from(index, balance);
     }
 }
